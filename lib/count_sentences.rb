69712135d0aa67853d66eeddc3aa7608358b71bd
emptycan <richardtrapanese@gmail.com>
2018-05-17 10:19:30 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # count sentences, if there are no sentences return 0, return the number
+    # sentences in a complex string
+    self.squeeze('.!?').count('.!?')
   end
 end
